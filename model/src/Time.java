a51b258d8d8cb6c649f0b07dd44246795fc42056
LineGuld <linekguld@hotmail.com>
2020-12-27 16:44:05 +0100

    Time

--- model/src/Time.java
+++ model/src/Time.java
@@ -1,3 +1,20 @@
 public class Time
 {
+  private int hour, minute, second;
+
+  public Time(int hour, int minute, int second)
+  {
+    //TODO validator
+
+    this.hour = hour;
+    this.minute = minute;
+    this.second = second;
+  }
+
+  public Time(int totalTimeInSeconds)
+  {
+    // TODO time in seconds
+  }
+
+
 }
