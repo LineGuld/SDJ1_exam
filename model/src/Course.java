c5d1fd9fd765bd128b176464910173ea026db533
LineGuld <linekguld@hotmail.com>
2020-12-29 14:17:31 +0100

    UML doneso

--- model/src/Course.java
+++ model/src/Course.java
@@ -29,12 +29,22 @@ private ArrayList<Lesson> schedule;
 
   public void removeLesson(Lesson lesson)
   {
-    // TODO remove lesson
+    for (Lesson lessons: schedule)
+    {
+      if (lessons.equals(lesson))
+        schedule.remove(lesson);
+    }
   }
 
   public boolean hasLessonOnDate(Date date)
   {
-    //TODO check lesson on date
+    boolean hasLesson = false;
+    for (Lesson lesson: schedule)
+    {
+     if (lesson.getDate().equals(date))
+     hasLesson = true;
+    }
+     return hasLesson;
   }
 
   public ArrayList<Lesson> getAllLessons()
@@ -54,5 +64,79 @@ private ArrayList<Lesson> schedule;
     return  topicArray;
   }
 
+  public ArrayList<OnlineLesson> getOnlineLessons()
+  {
+    ArrayList<OnlineLesson> onlineLessons = new ArrayList<OnlineLesson>();
 
+    for (Lesson lesson: schedule)
+    {
+      if (lesson instanceof OnlineLesson)
+      {
+        OnlineLesson onlinelesson = (OnlineLesson) lesson;
+        onlineLessons.add(onlinelesson);
+      }
+    }
+
+    return onlineLessons;
+  }
+
+  public ArrayList<SkypeLesson> getSkypeLessons()
+  {
+    ArrayList<SkypeLesson> skypeLessons = new ArrayList<SkypeLesson>();
+
+    for (Lesson lesson: schedule)
+    {
+      if (lesson instanceof SkypeLesson)
+      {
+        SkypeLesson skypeLesson = (SkypeLesson) lesson;
+        skypeLessons.add(skypeLesson);
+      }
+    }
+    return skypeLessons;
+  }
+
+  public ArrayList<DiscordLesson> getDiscordLessonsWithVoice()
+  {
+    ArrayList<DiscordLesson> voiceLessons = new ArrayList<DiscordLesson>();
+
+    for (Lesson lesson: schedule)
+    {
+      if (lesson instanceof DiscordLesson)
+      {
+        DiscordLesson discord = (DiscordLesson) lesson;
+        if (discord.usesVoiceChannel())
+          voiceLessons.add(discord);
+      }
+    }
+
+    return voiceLessons;
+  }
+
+  public ArrayList<File> getAllResources()
+  {
+    ArrayList<File> resources = new ArrayList<>();
+
+    for (Lesson lesson: schedule)
+    {
+      File[] files = lesson.getResources();
+      for (int i = 0; i < files.length; i++)
+      {
+        File res = files[i];
+        resources.add(res);
+      }
+    }
+
+    return resources;
+  }
+
+  public String toString()
+  {
+    String lessonString = "";
+    for (Lesson lesson: schedule)
+    {
+      lessonString += lesson.toString() + "\n";
+    }
+
+    return "Course: " + name + "\n" + lessonString;
+  }
 }
